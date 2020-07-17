950ffc2bf6f299e92d0d0a4afc823b38281d0b2e
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-07-17 13:05:09 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -23,5 +23,14 @@ def remove_strawberry(contacts)
       end
     end
   end
-  
+def remove_strawberry(contacts)
+  freddy = contacts["Freddy Mercury"]
+
+  freddy.each do |attribute, value|
+    if attribute == :favorite_icecream_flavors
+      value.delete_if{ |ice_cream| ice_cream == "strawberry" }
+    end
+  end
+  contacts
+end
 
