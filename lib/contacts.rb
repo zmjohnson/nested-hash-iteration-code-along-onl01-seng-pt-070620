878736a43d416ff0bebbd9d0dda14bc2d4b1525d
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-07-17 13:03:33 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -14,7 +14,7 @@ require 'pry'
   #   }
   # }
   
-def remove_strawberry(contacts)
+
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
     contact_details_hash.each do |attribute, data|
@@ -23,4 +23,5 @@ def remove_strawberry(contacts)
       end
     end
   end
-end
+  
+
